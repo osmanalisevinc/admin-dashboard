e6e718af23f555c3cb575b6bf01adb5d677803d8
osmanalisevinc <Sevinc32isparta@gmail.com>
2025-07-20 00:50:32 +0300

    added sidebar and navbar

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styles from "./sidebar.module.css"
- import {
+import React from "react";
+import styles from "./sidebar.module.css";
+import {
   MdDashboard,
   MdSupervisedUserCircle,
   MdShoppingBag,
@@ -12,7 +12,8 @@ import styles from "./sidebar.module.css"
   MdHelpCenter,
   MdLogout,
 } from "react-icons/md";
-import { auth, signOut } from "@/app/auth";
+import MenuLink from "./menulink/menuLink";
+import Image from "next/image";
 
 const menuItems = [
   {
@@ -78,8 +79,36 @@ const menuItems = [
 ];
 const Sidebar = () => {
   return (
-    <div className={styles.container}>Sidebar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src="/noavatar.png"
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetails}>
+          <span className={styles.username}>Osman Doe</span>
+          <span className={styles.userTitle}>Admin</span>
+        </div>
+      </div>
+      <ul>
+        {menuItems.map((cat) => (
+          <li key={cat.title} className={styles.list}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
