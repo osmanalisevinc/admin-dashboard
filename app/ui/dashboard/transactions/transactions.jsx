9784e7dcd6b48a1c09148fe8878d6f2f1a9431d6
osmanalisevinc <Sevinc32isparta@gmail.com>
2025-07-20 13:54:42 +0300

    add transaction

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
-import styles from "./transactions.module.css"
+import React from "react";
+import styles from "./transactions.module.css";
+import Image from "next/image";
 const Transactions = () => {
   return (
-    <div>Transactions</div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.user}>
+              <Image
+                src="/noavatar.png"
+                alt=""
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>11.11.2025</td>
+            <td>$3000</td>
+          </tr>
+          <tr>
+            <td className={styles.user}>
+              <Image
+                src="/noavatar.png"
+                alt=""
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>11.11.2025</td>
+            <td>$3000</td>
+          </tr>
+          <tr>
+            <td className={styles.user}>
+              <Image
+                src="/noavatar.png"
+                alt=""
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>11.11.2025</td>
+            <td>$3000</td>
+          </tr>
+          <tr>
+            <td className={styles.user}>
+              <Image
+                src="/noavatar.png"
+                alt=""
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>11.11.2025</td>
+            <td>$3000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Transactions
+export default Transactions;
