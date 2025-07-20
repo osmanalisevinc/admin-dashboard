22cb9d28a8460fd0ceed4cc7844902ad374ddf62
osmanalisevinc <Sevinc32isparta@gmail.com>
2025-07-20 12:31:22 +0300

    add card

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import styles from "./card.module.css"
+import React from "react";
+import styles from "./card.module.css";
+import { MdSupervisedUserCircle } from "react-icons/md";
 
 const Card = () => {
   return (
-    <div className={styles.container}>Card</div>
-  )
-}
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={32} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total users</span>
+        <span className={styles.number}>10,234</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12% </span>more than previous week
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
